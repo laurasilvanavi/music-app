f4ea22282d1119b3b06afb1f36ebfc2fc445267f
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2019-09-30 23:26:10 +0300

    feat(first beep): added tone.js dependency and generated first sound

---
+++ src/app/instrument/instrument.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import * as Tone from 'tone';
+
+@Component({
+  selector: 'app-instrument',
+  templateUrl: './instrument.component.html',
+  styleUrls: ['./instrument.component.scss']
+})
+export class InstrumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  playSound() {
+    const synth = new Tone.Synth().toMaster();
+    synth.triggerAttackRelease('C4', '8n');
+  }
+
+}
