ffc555972b2b49346e1ad233336d918345b6b867
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2019-11-10 02:21:06 +0200

    refactor: added loop for notes

--- src/app/instrument/instrument.component.ts
+++ src/app/instrument/instrument.component.ts
@@ -7,14 +7,17 @@ import * as Tone from 'tone';
   styleUrls: ['./instrument.component.scss']
 })
 export class InstrumentComponent implements OnInit {
+  notes: string[];
+  synth: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.synth = new Tone.Synth().toMaster();
+    this.notes = ['C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3', 'A3', 'B3', 'C4'];
   }
 
   playNote(note: string) {
-    const synth = new Tone.Synth().toMaster();
-    synth.triggerAttackRelease(note, "8n");
+    this.synth.triggerAttackRelease(note, '8n');
   }
 }
