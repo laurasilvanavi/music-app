175651292cce8f6d398438e56503ef2803cb9282
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2020-07-30 01:05:51 +0300

    feat: implemented musicVAE for new music creation with AI

--- src/app/ai-instrument/ai-instrument.component.ts
+++ src/app/ai-instrument/ai-instrument.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mm from '@magenta/music';
-import { JUMP_SONG} from '../notes.const';
+import { JUMP_SONG } from '../notes.const';
 
 @Component({
   selector: 'app-ai-instrument',
@@ -11,6 +11,7 @@ export class AiInstrumentComponent implements OnInit {
 
   player;
   musicRNN;
+  musicVAE;
   steps = 60; // duration
   temperature = 1.5; // randomization
   stepsPerQuarter = 4; // tempo
@@ -20,6 +21,10 @@ export class AiInstrumentComponent implements OnInit {
     // Hosted Checkpoints: https://github.com/magenta/magenta-js/blob/master/music/checkpoints/README.md
     this.musicRNN = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
     this.musicRNN.initialize();
+
+    // this.musicVAE = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/multitrack');
+    this.musicVAE = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
+    this.musicVAE.initialize();
   }
 
   playOriginalMelody() {
@@ -41,5 +46,15 @@ export class AiInstrumentComponent implements OnInit {
         .then((sample) => this.player.start(sample))
         .catch(err => console.log(err));
     }
+  }
+
+  playNewMelodyWithAI() {
+    if (this.player.isPlaying()) {
+      this.player.stop();
+      return;
+    }
+    this.musicVAE
+      .sample(1, this.temperature)
+      .then((sample) => this.player.start(sample[0]));
   }
 }
